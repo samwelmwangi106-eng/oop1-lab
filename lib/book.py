2303738418650fb407a80568e6558d847fa6c55b
Samwel <samwelmwangi106@gmail.com>
2026-06-06 10:56:23 +0300

    updated

--- lib/book.py
+++ lib/book.py
@@ -1,45 +1,42 @@
 #!/usr/bin/env python3
 
 class Book:
-    def __init__(self,title,page_count):
+    def __init__(self, title, page_count):
         self.title = title
         self.page_count = page_count
 
     @property
     def page_count(self):
         return self._page_count
-    
+
     @page_count.setter
-    def page_count(self,value):
-        if not isinstance(value,int):
-            print("page_count must be an interger")
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
         else:
             self._page_count = value
 
     def turn_page(self):
         print("Flipping the page...wow, you read fast!")
-
 class Coffee:
-    def __init__(self,size,price):
+    def __init__(self, size, price):
         self.size = size
         self.price = price
 
     @property
     def size(self):
         return self._size
-    
+
     @size.setter
     def size(self, value):
-        if value not in ["small","Medium","Large"]:
-            print("size must be small,medium or large")
-        
+        if value not in ["Small", "Medium", "Large"]:
+            print("size must be Small, Medium, or Large")
         else:
             self._size = value
 
-
     def tip(self):
-        print("This coffee is great,her's a tip!")
-        self.price +=1
+        print("This coffee is great, here's a tip!")
+        self.price += 1
 # Example usage
 book = Book("Python Basics", 200)
 book.turn_page()
